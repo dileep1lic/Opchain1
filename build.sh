cab2d3b6e67dfd585f1ca3c0e9afed0293672e13
dileep2lic <168991807+dileep2lic@users.noreply.github.com>
2026-02-25 09:04:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
 # python manage.py run_sync_async
