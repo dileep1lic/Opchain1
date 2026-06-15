a07864da40868716869796ea3510f5a80744cf4b
Dileep Kumar Khrashiya <dileep1lic@gmail.com>
2026-06-15 20:35:50 +0530

    test deta con

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,7 +123,7 @@ if _db_url:
     DATABASES = {
         'default': dj_database_url.config(
             default=_db_url,
-            conn_max_age=600,
+            conn_max_age=0,
             conn_health_checks=True,
             ssl_require=True,
         )
